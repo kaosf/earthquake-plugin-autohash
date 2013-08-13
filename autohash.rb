421644fe3d6bcfd70e11c28057c49ea15f5a10cc
ka <ka.kaosf@gmail.com>
2013-08-13 12:14:13 +0900

    refactor for test

--- autohash.rb
+++ autohash.rb
@@ -1,18 +1,13 @@
-Earthquake.init do
+class HashTagsManager
   FILEPATH ||= "#{ENV['HOME']}/.earthquake/autohash"
 
-  command :autohash do
-    File.delete FILEPATH if File.exists? FILEPATH
-  end
-
-  command :autohash do |m|
-    hashtags = m[1].split("\s")
+  def save(hashtags)
     File.open FILEPATH, 'w' do |f|
       f.puts hashtags
     end
   end
 
-  command %r|^[^:\$].*| do |m|
+  def get
     hash_str = ""
     if File.exists? FILEPATH
       File.open FILEPATH do |f|
@@ -22,6 +17,26 @@ Earthquake.init do
         end
       end
     end
-    input(":update #{m[0]}#{hash_str}")
+    hash_str
+  end
+
+  def reset
+    File.delete FILEPATH if File.exists? FILEPATH
+  end
+end
+
+Earthquake.init do
+  h = HashTagsManager.new
+
+  command :autohash do
+    h.reset
+  end
+
+  command :autohash do |m|
+    h.save m[1].split("\s")
+  end
+
+  command %r|^[^:\$].*| do |m|
+    input(":update #{m[0]}#{h.get}")
   end
 end
